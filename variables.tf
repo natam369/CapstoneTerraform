568518f702e992270cf7b071df505509b0a1d1c2
natam369 <58681656+natam369@users.noreply.github.com>
2020-02-27 10:46:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "key_name" {
-  default = "meghanamumbai"
+  default = "capstone"
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/mkey.pem"
+  default = "/root/.ssh/bikee.pem"
 }
 
 variable "us-east-zones" {
-  default = ["us-east-1a", "us-east-1b"]
+  default = ["us-west-2a", "us-west-2b"]
 }
 
 variable "sg-id" {
-  default = "sg-0eb024eb164eb86c0"
+  default = "sg-0283912e679692daa"
 }
